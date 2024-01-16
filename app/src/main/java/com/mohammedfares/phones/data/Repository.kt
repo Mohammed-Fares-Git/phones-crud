50c58db317b95e9f404bb105fadcdc2efbac5976
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-16 19:56:30 +0100

    create implementation for the repo

--- app/src/main/java/com/mohammedfares/phones/data/Repository.kt
+++ app/src/main/java/com/mohammedfares/phones/data/Repository.kt
@@ -1,4 +1,25 @@
 package com.mohammedfares.phones.data
 
-class Repository {
+import com.mohammedfares.phones.domain.IRepository
+import javax.inject.Inject
+
+class Repository @Inject constructor(
+    val dao: PhoneDao
+): IRepository {
+    override fun getAll(): List<Phone> {
+        return dao.selectAll()
+    }
+    override fun getById(id: Long): Phone? {
+        return dao.selectById(id)
+    }
+    override suspend fun delete(phone: Phone): Int {
+        return dao.delete(phone)
+    }
+    override suspend fun update(phone: Phone): Int {
+        return dao.update(phone)
+    }
+    override suspend fun create(phone: Phone): Long {
+        return dao.inert(phone)
+    }
+
 }
