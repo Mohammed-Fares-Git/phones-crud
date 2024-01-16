259aee90726efa62f1e46c99826d6fcf35acf34a
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-16 19:58:24 +0100

    create phone entity

--- app/src/main/java/com/mohammedfares/phones/data/Phone.kt
+++ app/src/main/java/com/mohammedfares/phones/data/Phone.kt
@@ -1,3 +1,11 @@
 package com.mohammedfares.phones.data
 
-data class Phone()
+import androidx.room.Entity
+
+@Entity(tableName = "mobile_phones")
+data class Phone(
+    val id: Long,
+    val name: String,
+    val price: Int,
+    val image: String
+)
