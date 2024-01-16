c6765456fbe5409be8b851dead1ca0bdac1337b8
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-16 19:57:57 +0100

    create dao

--- app/src/main/java/com/mohammedfares/phones/data/PhoneDao.kt
+++ app/src/main/java/com/mohammedfares/phones/data/PhoneDao.kt
@@ -1,4 +1,21 @@
 package com.mohammedfares.phones.data
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
 interface PhoneDao {
+    @Insert
+    suspend fun inert(phone: Phone): Long
+    @Delete
+    suspend fun delete(phone: Phone): Int
+    @Update
+    suspend fun update(phone: Phone): Int
+    @Query("SELECT * FROM mobile_phones")
+    fun selectAll(): List<Phone>
+    @Query("SELECT * FROM mobile_phones WHERE id = :id")
+    fun selectById(id: Long): Phone
 }
