4be37fa44ad17b335368c1fdfbb64bb42f59c2d6
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-16 19:55:45 +0100

    provide the dependencies in appmodule

--- app/src/main/java/com/mohammedfares/phones/di/AppModule.kt
+++ app/src/main/java/com/mohammedfares/phones/di/AppModule.kt
@@ -1,4 +1,33 @@
 package com.mohammedfares.phones.di
 
+import android.content.Context
+import androidx.room.Room
+import com.mohammedfares.phones.data.PhoneDao
+import com.mohammedfares.phones.data.PhonesDb
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesRoomDb(@ApplicationContext ctx: Context): PhonesDb{
+        return Room.databaseBuilder(
+            ctx,
+            PhonesDb::class.java,
+            "phones_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesPhoneDAo(phonesDb: PhonesDb): PhoneDao{
+        return phonesDb.phoneDao();
+    }
 }
