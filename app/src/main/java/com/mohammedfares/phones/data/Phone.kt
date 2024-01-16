3e02c52d066c29b597bbbb405573751ab13c85d3
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-16 20:37:24 +0100

    rank fieald to db

--- app/src/main/java/com/mohammedfares/phones/data/Phone.kt
+++ app/src/main/java/com/mohammedfares/phones/data/Phone.kt
@@ -1,11 +1,14 @@
 package com.mohammedfares.phones.data
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "mobile_phones")
 data class Phone(
+    @PrimaryKey(autoGenerate = true)
     val id: Long,
     val name: String,
     val price: Int,
-    val image: String
+    val image: String,
+    val rank: Int = 1
 )
