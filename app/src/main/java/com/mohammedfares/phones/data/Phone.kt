5056109a968033effc2b1e1a2240bd17fb73bae6
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-17 20:07:39 +0100

    make the id field nullable

--- app/src/main/java/com/mohammedfares/phones/data/Phone.kt
+++ app/src/main/java/com/mohammedfares/phones/data/Phone.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "mobile_phones")
 data class Phone(
     @PrimaryKey(autoGenerate = true)
-    val id: Long,
+    val id: Long? = null,
     val name: String,
     val price: Int,
     val image: String,
