36854a841984162e512f8de8677747fa7e4b64ad
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-17 20:02:23 +0100

    create search items

--- app/src/main/java/com/mohammedfares/phones/ui/components/Search.kt
+++ app/src/main/java/com/mohammedfares/phones/ui/components/Search.kt
@@ -1,4 +1,25 @@
 package com.mohammedfares.phones.ui.components
 
-class Search {
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mohammedfares.phones.data.Phone
+
+@Composable
+fun SearchItems(names: List<String>) {
+    names.forEach {
+        Row (modifier = Modifier.fillMaxWidth()){
+            Icon(imageVector = Icons.Filled.History, contentDescription = "History icon", modifier = Modifier.padding(end = 10.dp))
+            Text(text = it, fontSize = 20.sp)
+        }
+    }
 }
