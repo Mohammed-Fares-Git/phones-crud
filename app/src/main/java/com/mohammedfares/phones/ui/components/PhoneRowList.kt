b074a0f0a5b0c9e3f97a3006ff69a50e94e92a33
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-17 20:03:32 +0100

    create horizontal phones list

--- app/src/main/java/com/mohammedfares/phones/ui/components/PhoneRowList.kt
+++ app/src/main/java/com/mohammedfares/phones/ui/components/PhoneRowList.kt
@@ -1,25 +1,22 @@
 package com.mohammedfares.phones.ui.components
 
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.mohammedfares.phones.data.Phone
 
 @Composable
-fun PhoneGridList() {
-
-}
-
-
-@Preview
-@Composable
-fun PhoneGridListPreview(nav: NavHostController, phones: List<Phone>) {
-    LazyVerticalGrid(columns = GridCells.Fixed(2), content ={
+fun PhoneRowList(nav: NavHostController, phones: List<Phone>) {
+    LazyRow {
         itemsIndexed(phones) {index, item ->
             PhoneGridItem(nav = nav, phone = item)
         }
-    })
+    }
 }
+
+
