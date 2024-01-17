2448419951daaa89ce7c884581c15b55a77b28c9
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-01-17 20:05:19 +0100

    create grid pphone item

--- app/src/main/java/com/mohammedfares/phones/ui/MainViewModel.kt
+++ app/src/main/java/com/mohammedfares/phones/ui/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.mohammedfares.phones.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.MohammedFares.ecomerce_project.comon.Resource
 import com.mohammedfares.phones.data.Phone
 import com.mohammedfares.phones.data.Repository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
@@ -14,8 +17,41 @@ class MainViewModel @Inject constructor(
     private val _phonesListMutable = MutableStateFlow<List<Phone>>(emptyList())
     val phonesListMutable = _phonesListMutable.asStateFlow()
 
+    private val _namesMutableStateFlow = MutableStateFlow<List<String>>(emptyList())
+    val namesStateFlow = _namesMutableStateFlow.asStateFlow()
+
+
+    private val _addStaeFlow = MutableStateFlow<Resource<Long>>(Resource.Empty())
+    val addStateFlow = _addStaeFlow.asStateFlow()
+
 
     init {
-        _phonesListMutable.value = repo.getAll()
+        getAll()
     }
+
+    fun getAll() {
+        viewModelScope.launch {
+            _phonesListMutable.value = repo.getAll()
+        }
+    }
+
+    fun getNames(name: String) {
+        viewModelScope.launch {
+            _namesMutableStateFlow.value = repo.search(name)
+        }
+    }
+
+    fun addPhone(phone: Phone) {
+        viewModelScope.launch {
+            val created = repo.create(phone)
+
+            if (created != (-1).toLong()){
+                _addStaeFlow.value = Resource.Success(created)
+            } else {
+                _addStaeFlow.value = Resource.Error("fail to add")
+            }
+        }
+    }
+
+
 }
